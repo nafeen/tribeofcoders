b3448df4d815d68d95de02f080d9af227d40ccd2
Nafeen <abdulnafeen@yahoo.com>
2021-08-08 22:45:23 +0530

    CORRECTED state object in VueX store. ADDED setTitle action.

--- store/index.js
+++ store/index.js
@@ -1,8 +1,11 @@
-export const state = () => {
+export const state = () => ({
   title: ''
-}
+})
 export const mutations = {
   setTitle: (state, title) => state.title = title
+}
+export const actions = {
+  setTitle: (state, title) => state.commit('setTitle', title)
 }
 export const getters = {
   getTitle: state => state.title
